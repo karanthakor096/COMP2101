63f1dae65a3736af10fce725c3043a48d93602b8
karanthakor096 <thakor.karan123@outlook.com>
2021-04-15 12:33:38 -0400

    Update lab3.ps1

--- Powershell/lab3.ps1
+++ Powershell/lab3.ps1
@@ -1,10 +1,12 @@
 Get-CimInstance Win32_NetworkAdapterConfiguration |
  Where-Object IPEnabled -EQ "True" |
-    Select-Object -Property @{n = "Index"; e={$_.Index}},
-         @{n = "IP Address"; e={$_.ipaddress}},
+    Select-Object -Property @{n ="Description";e={$_.Description}},
+        @{n = "Index"; e={$_.Index}},
+        @{n = "IP Address"; e={$_.ipaddress}},
         @{n = "Subnet Mask"; e={$_.Ipsubnet}},
         @{n = "DNS Name"; e={$_.dnsdomainname}},
         @{n = "DNS Server"; e={$_.DNSServersearchorder}} | 
 Format-Table
+         
 
                             
